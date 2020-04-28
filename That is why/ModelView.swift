a867aedf215badbdb6be2015961287791f41bf3a
Vitalii Sosin <375693@mail.ru>
2020-04-28 15:43:59 +0300

    create tableView

---
+++ That is why/ModelView.swift
@@ -0,0 +1,23 @@
+//
+//  ModelView.swift
+//  That is why
+//
+//  Created by Виталий Сосин on 28.04.2020.
+//  Copyright © 2020 Vitalii Sosin. All rights reserved.
+//
+
+import UIKit
+
+class ModelView: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
--- That is why/ViewController.swift
+++ That is why/ViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var textDefault = ""
+    
+    @IBOutlet weak var screenLabel: UILabel!
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var buttonOutlet: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    var textFieldActionQ = ""
+    var switchYouQ = "Вас"
+    var switchHelthyQ = ""
+    
+    @IBAction func textFieldAction(_ sender: UITextField) {
+        textFieldActionQ = sender.text!
+    }
+    @IBAction func switchYou(_ sender: UISwitch) {
+        if sender.isOn {
+            switchYouQ = "Тебя"
+        } else {
+            switchYouQ = "Вас"
+        }
+    }
+    
+    // Уважаемый
+    @IBAction func switchRespected(_ sender: UISwitch) {
+    }
+    
+    // Здоровья
+    @IBAction func switchHelthy(_ sender: UISwitch) {
+        
+    }
+    
+    // Счастья
+    @IBAction func switchHappy(_ sender: UISwitch) {
+    }
+    
+    // Денег
+    @IBAction func switchMoney(_ sender: UISwitch) {
+    }
+    
+    // Смайл
+    @IBAction func switchSmile(_ sender: UISwitch) {
+    }
+    
+    // Семейного благополучия
+    @IBAction func switchFamily(_ sender: UISwitch) {
+    }
+    
+    // Чтобы все задуманное сбылось
+    @IBAction func switchWill(_ sender: UISwitch) {
+    }
+    // Карьерного роста
+    @IBAction func switchBussines(_ sender: UISwitch) {
+    }
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        textDefault = "Добрый день \(textFieldActionQ)! Поздравляю \(switchYouQ) с Вашим Новым Годом рождения. Желаю "
+        screenLabel.text = textDefault
+        
+    }
 }
-
